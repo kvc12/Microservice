a8000311cbf88fbe850680a2d465dcc8fb8a9db9
keval.chheda <keval.chheda@ITLNB742.kiya.ai>
2024-07-16 10:36:40 +0530

    feign client configuration modified

--- microservice1/src/main/java/com/infra/microservice/fiegnClient/ServiceBClient.java
+++ microservice1/src/main/java/com/infra/microservice/fiegnClient/ServiceBClient.java
@@ -5,10 +5,10 @@ import java.util.Map;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "serviceb")
+@FeignClient(name = "serviceb", url = "http://localhost:8080")
 public interface ServiceBClient {
-	
-	@GetMapping("/data")
-	public Map<String, String> getData();
 
+    @GetMapping("/serviceb/data")
+    Map<String, String> getData();
 }
+
